bdce740b2d7f06a207ac23c0d8fa25db9b239781
mina remon shaker <39167278+mina20088@users.noreply.github.com>
2019-01-13 05:07:53 +0200

    Create Array.js

---
+++ Array.js
@@ -0,0 +1,12 @@
+function conversion(){
+  var x = []
+  var input;
+  var convert; 
+  var i = 0;
+  label:for(i = 0; i< 3; i++){
+     input = eval((prompt("please enter the arrays")));
+     convert = x.push(input);
+    }
+    return x;
+}
+console.log(x)
